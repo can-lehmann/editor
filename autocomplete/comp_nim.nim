f3c59297dc8ce5ed369e155c7fd79ff6536e9f3a
josh <josh.leh.2018@gmail.com>
2020-08-02 14:59:16 +0200

    Improve autocompletion speed

--- autocomplete/comp_nim.nim
+++ autocomplete/comp_nim.nim
@@ -46,6 +46,7 @@ type
     log: Log
     folder: string
     file_id: int
+    max_completions: int
     
     tracked: HashSet[Buffer]
     case_styles: Table[Buffer, CaseStyle]
@@ -195,7 +196,8 @@ proc make_context(log: Log): Context =
       Rune(')'), Rune('}'), Rune(']'), Rune('{')
     ],
     min_word_len: 5,
-    log: log
+    log: log,
+    max_completions: 128
   )
   when not defined(windows):
     result.stdout_selector = new_selector[pointer]()
@@ -239,20 +241,30 @@ proc exec(ctx: Context, job: Job) {.async.} =
             y: parse_int(parts[5]) - 1
           )
         ))
+      ctx.log.add_info("comp_nim", "Received " & $defs.len & " definitions")
       job.defs_cb(defs)
       ctx.case_styles[job.buffer] = defs.detect_case_style()
     of JobComp:
       let socket = new_async_socket()
       await socket.connect("127.0.0.1", ctx.port)
-      let
-        temp = await ctx.save_temp_buffer(job.buffer)
-        pos = job.buffer.to_2d(job.comp_pos)
-      await socket.send("sug " & job.buffer.file_path & ";" & temp & ":" & $(pos.y + 1) & ":" & $pos.y & "\n")
+      let pos = job.buffer.to_2d(job.comp_pos)
+      if job.buffer.len != 0:
+        let temp = await ctx.save_temp_buffer(job.buffer)
+        await socket.send("sug " & job.buffer.file_path & ";" & temp & ":" & $(pos.y + 1) & ":" & $pos.y & "\n")
+      else:
+        await socket.send("sug " & job.buffer.file_path & ":" & $(pos.y + 1) & ":" & $pos.y & "\n")
       let lines = await socket.recv_lines()
       socket.close()
       
       var comps: seq[Completion]
       for line in lines:
+        if comps.len >= ctx.max_completions:
+          if lines.len >= ctx.max_completions:
+            ctx.log.add_warning("comp_nim",
+              "Received " & $lines.len &
+              " completions but the maximum is " & $ctx.max_completions
+            )
+          break
         let parts = line.strip(chars={'\r', '\n'}).split("\t")
         if parts.len < 3:
           continue
@@ -266,6 +278,7 @@ proc exec(ctx: Context, job: Job) {.async.} =
           kind: to_comp_kind(parts[1]),
           text: styled
         ))
+      ctx.log.add_info("comp_nim", "Completion count: " & $comps.len)
       job.comp_cb(comps)
 
 proc enqueue(ctx: Context, job: Job) =
@@ -294,7 +307,8 @@ proc restart_nimsuggest(ctx: Context) =
   let project_buffer = ctx.tracked.peek()
   ctx.is_restarting = true
   ctx.nimsuggest = start_process(find_exe("nimsuggest"), args=[
-    "--autobind", "--address:127.0.0.1", project_buffer.file_path
+    "--autobind", "--address:127.0.0.1", project_buffer.file_path,
+    "--maxresults:" & $ctx.max_completions
   ])
   when defined(windows):
     ctx.read_port()
@@ -332,7 +346,13 @@ proc exec_waiting(ctx: Context) {.async.} =
 
 method poll(ctx: Context) =
   if has_pending_operations():
-    drain(0)
+    try:
+      let start = get_time()
+      drain(1)
+      let diff = get_time() - start
+      ctx.log.add_info("comp_nim", "Call to drain took " & $diff)
+    except OSError as err:
+      ctx.log.add_error("comp_nim", err.msg)
   
   when not defined(windows):
     if ctx.is_restarting and ctx.nimsuggest.running:
